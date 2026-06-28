75514a2b90243e6d3b97cdad2ea8a7b53fbfc2c8
Aryan Gore <aryangore130@gmail.com>
2026-06-28 19:01:08 +0530

    responsive

--- script.js
+++ script.js
@@ -43,3 +43,25 @@ const observer=new IntersectionObserver(entries=>{
   entries.forEach(e=>{if(e.isIntersecting){e.target.classList.add('visible')}});
 },{threshold:0.15});
 reveals.forEach(r=>observer.observe(r));
+
+
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+
+hamburger.addEventListener("click", () => {
+
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+
+});
+
+document.querySelectorAll(".nav-menu a").forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+
+    });
+
+});
